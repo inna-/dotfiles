7eac09b0d6c9f1853b20db298911976f2d9eea41
Inna Kim <inneekim@gmail.com>
2015-07-29 20:28:58 +0900

    debugging stuff

--- programs.sh
+++ programs.sh
@@ -4,4 +4,11 @@ sudo apt-get -y install \
 			tmux \
 			vim-gtk \
 			chromium-browser \
+            playonlinux \
+            wine \
+            calibre \
+            amarok \
+            shutter \
+            gdb \
+            build-essential \
             
